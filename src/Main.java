3e7159da45525ea2e510d226b992a3b501339b63
Brujitari <natimontesg2@mail.com>
2022-08-10 04:49:14 +0200

    Getters and setters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static class Persona {
+        private int age;
+        private String name;
+        private int phone;
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public int getAge() {
+            return this.age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return this.name;
+        }
+
+        public void setPhone(int phone) {
+            this.phone = phone;
+        }
+        public int getPhone() {
+            return this.phone;
+        }
     }
+
+
+
+    public static void main(String[] args) {
+        Persona miPersona = new Persona();
+        miPersona.setAge(4);
+        miPersona.setName("Nat");
+        miPersona.setPhone(465464646);
+
+        int age = miPersona.getAge();
+        String name = miPersona.getName();
+        int phone = miPersona.getPhone();
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(phone);
+    }
+
 }
